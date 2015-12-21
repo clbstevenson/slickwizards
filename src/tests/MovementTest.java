f8b9fa6cfac6e0470c21576c1654312460d797e7
exovum <stevecal@mail.gvsu.edu>
2015-12-21 15:57:20 -0500

    MovementTest now works locally

--- src/tests/MovementTest.java
+++ src/tests/MovementTest.java
@@ -42,9 +42,9 @@ public class MovementTest extends BasicGame {
 		// TODO Auto-generated method stub
 		images = new Image[5];
 
-		moves = new SpriteSheet("testRes/testMovement2.png", 32, 32);
+		moves = new SpriteSheet("res/tests_res/testMovement2.png", 32, 32);
 		player = moves.getSprite(0, 0);
-		map = new Image("testRes/testMap1.png");
+		map = new Image("res/tests_res/testMap1.png");
 
 		mapID = new Vector2f(0, 1);
 		mapFrames = new Image[3][3];
@@ -56,9 +56,9 @@ public class MovementTest extends BasicGame {
 
 		// TODO: Make a loop to initialize the Tiled maps
 		tiledMapArr = new TiledMap[1][3];
-		tiledMapArr[0][0] = new TiledMap("testRes/map0-0.tmx");
-		tiledMapArr[0][1] = new TiledMap("testRes/map0-1.tmx");
-		tiledMapArr[0][2] = new TiledMap("testRes/map0-2.tmx");
+		tiledMapArr[0][0] = new TiledMap("res/tests_res/map0-0.tmx");
+		tiledMapArr[0][1] = new TiledMap("res/tests_res/map0-1.tmx");
+		tiledMapArr[0][2] = new TiledMap("res/tests_res/map0-2.tmx");
 
 		int x = (gc.getWidth() / 2) - (player.getWidth() / 2);
 		int y = (gc.getHeight() / 2) - (player.getHeight() / 2);
